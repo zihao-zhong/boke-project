e045651b50205c3226e6a3e1b09a5adfdecfe45f
zihao-web <1095996920@qq.com>
2021-01-07 00:29:01 +0800

    增加压缩配置

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,5 +1,9 @@
 const path = require('path') 
 const env = require('./env')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
+
 
 function resolve(dir){
   return path.join(__dirname,dir) 
@@ -22,6 +26,21 @@ module.exports = {
       .set('@', resolve('./src'))
       .set('@api', resolve('./src/api'))
       //set第一个参数：设置的别名，第二个参数：设置的路径
+  },
+  configureWebpack: config => {
+    const plugins = [];
+    if (IS_PROD) {
+      plugins.push(
+        new CompressionWebpackPlugin({
+          filename: "[path].gz[query]",
+          algorithm: "gzip",
+          test: productionGzipExtensions,
+          threshold: 5000,
+          minRatio: 0.8
+        })
+      );
+    }
+    config.plugins = [...config.plugins, ...plugins];
   },
   devServer: {
     // overlay: { // 让浏览器 overlay 同时显示警告和错误
